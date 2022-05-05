da8ef3a94201eba559102710b3c1753d7d957880
devopsbyraja <rajakullayappa231@gmail.com>
2022-05-05 21:52:34 +0530

    my first commit

---
+++ src/main/java/com/kvtech/app/App.java
@@ -0,0 +1,13 @@
+package com.kvtech.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
